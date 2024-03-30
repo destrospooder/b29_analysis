233b49ced6cbb2a3f24ac8c024472ca2c2e99896
Ben Aziel <70983779+destrospooder@users.noreply.github.com>
2024-03-29 20:57:00 -0400

    comment

--- 1_thin_airfoil_theory.py
+++ 1_thin_airfoil_theory.py
@@ -2,3 +2,4 @@
 
 # Roughly analogous to HW 4 Problem 4
 
+# reformatting this weekend to increase modularity of code and make future assignments easier
--- 2_finite_wing_theory.py
+++ 2_finite_wing_theory.py
@@ -2,3 +2,4 @@
 
 # Roughly analogous to HW 5 Problem 3
 
+# reformatting this weekend to increase modularity of code and make future assignments easier
