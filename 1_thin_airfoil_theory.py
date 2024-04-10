59890d417217ea9133d58db9c5d2344b151ebba0
Ben Aziel <benjamin.aziel@cooper.edu>
2024-04-10 18:14:45 -0400

    finished thin airfoil theory
    should be good now. zero lift aoa's look a bit low but will check over in a bit

--- 1_thin_airfoil_theory.py
+++ 1_thin_airfoil_theory.py
@@ -3,6 +3,74 @@
 # Roughly analogous to HW 4 Problem 4
 
 import numpy as np
+import sympy as sp
+import math as math
 import matplotlib.pyplot as plt
 from parameters import *
 
+alpha = np.linspace(-np.deg2rad(10), np.deg2rad(10), 100)
+b29_root.thin_airfoil_theory(alpha, load = 'b29_root')
+b29_tip.thin_airfoil_theory(alpha, load = 'b29_tip')
+
+# to redo fourier coeff integration, uncomment
+# b29_root.thin_airfoil_theory(alpha, save = 'b29_root')
+# b29_tip.thin_airfoil_theory(alpha, save = 'b29_tip')
+
+fig, ax = plt.subplots(1, 2, figsize=(12, 6))
+fig.suptitle('Cl and Cm_LE for b29 root airfoil')
+
+ax1 = ax[0]
+ax1.plot(alpha, b29_root.Cl, color = 'k')
+ax1.plot(-(b29_root.Cl[0] - ((b29_root.Cl[-1] - b29_root.Cl[0])/(alpha[-1] - alpha[0])) * alpha[0]) /  ((b29_root.Cl[-1] - b29_root.Cl[0])/(alpha[-1] - alpha[0])), 0, 'ro', label = 'alpha_L0 =' + str(math.degrees(-(b29_root.Cl[0] - ((b29_root.Cl[-1] - b29_root.Cl[0])/(alpha[-1] - alpha[0])) * alpha[0]) /  ((b29_root.Cl[-1] - b29_root.Cl[0])/(alpha[-1] - alpha[0])))) + ' degrees')
+ax1.set_xlabel('alpha [rad]')
+ax1.set_ylabel('Cl')
+ax1.legend()
+ax1.set_title('Cl vs. alpha')
+
+ax2 = ax[1]
+ax2.plot(alpha, b29_root.Cm_LE, color = 'k')
+ax2.set_xlabel('alpha [rad]')
+ax2.set_ylabel('Cm_LE')
+ax2.set_title('Cm_LE vs. alpha')
+plt.tight_layout()
+plt.savefig('plots/b29_root_2d_airfoil_behavior.png')
+plt.show()
+
+fig, ax = plt.subplots(1, 2, figsize=(12, 6))
+fig.suptitle('Cl and Cm_LE for b29 tip airfoil')
+
+ax1 = ax[0]
+ax1.plot(alpha, b29_tip.Cl, color = 'k')
+ax1.plot(-(b29_tip.Cl[0] - ((b29_tip.Cl[-1] - b29_tip.Cl[0])/(alpha[-1] - alpha[0])) * alpha[0]) /  ((b29_tip.Cl[-1] - b29_tip.Cl[0])/(alpha[-1] - alpha[0])), 0, 'ro', label = 'alpha_L0 =' + str(math.degrees(-(b29_tip.Cl[0] - ((b29_tip.Cl[-1] - b29_tip.Cl[0])/(alpha[-1] - alpha[0])) * alpha[0]) /  ((b29_tip.Cl[-1] - b29_tip.Cl[0])/(alpha[-1] - alpha[0])))) + ' degrees')
+ax1.set_xlabel('alpha [rad]')
+ax1.set_ylabel('Cl')
+ax1.legend()
+ax1.set_title('Cl vs. alpha')
+
+ax2 = ax[1]
+ax2.plot(alpha, b29_tip.Cm_LE, color = 'k')
+ax2.set_xlabel('alpha [rad]')
+ax2.set_ylabel('Cm_LE')
+ax2.set_title('Cm_LE vs. alpha')
+plt.tight_layout()
+plt.savefig('plots/b29_tip_2d_airfoil_behavior.png')
+plt.show()
+
+fig, ax = plt.subplots(1, 2, figsize=(12, 6))
+fig.suptitle('Fl for b29 root, b29 tip')
+
+ax1 = ax[0]
+ax1.plot(alpha, 1/2 * b29_root.Cl * density * (plane.cruise_speed**2) * wing.wing_area, color = 'k')
+ax1.set_xlabel('alpha [rad]')
+ax1.set_ylabel('Fl [N]')
+ax1.legend()
+ax1.set_title('Fl vs. alpha, b29_root')
+
+ax2 = ax[1]
+ax2.plot(alpha, 1/2 * b29_tip.Cl * density * (plane.cruise_speed**2) * wing.wing_area, color = 'k')
+ax2.set_xlabel('alpha [rad]')
+ax2.set_ylabel('Fl [N]')
+ax2.set_title('Fl vs. alpha, b29_tip')
+plt.tight_layout()
+plt.savefig('plots/lift_force_comparison.png')
+plt.show()
--- parameters.py
+++ parameters.py
@@ -1,10 +1,11 @@
 #!/bin/env python
 
 import numpy as np
-import scipy as sci
+import sympy as sp
+from scipy.interpolate import CubicSpline
 
 class Airfoil:
-    def __init__(self, chord_length, max_thickness, max_thickness_loc, max_camber, max_camber_loc,):
+    def __init__(self, chord_length, max_thickness, max_thickness_loc, max_camber, max_camber_loc, name = None):
         self.chord_length = chord_length
         self.max_thickness = max_thickness # t
         self.max_thickness_loc = max_thickness_loc
@@ -17,9 +18,39 @@ class Airfoil:
         self.y_coords = self.chord_length * coords[:, 1]
 
         self.xs = np.linspace(0, self.chord_length, 1000)
-        self.top_curve = (sci.interpolate.CubicSpline(self.x_coords[:19], self.y_coords[:19]))(self.xs)
-        self.bottom_curve = (sci.interpolate.CubicSpline(self.x_coords[19:], self.y_coords[19:]))(self.xs)
+        self.top_curve = (CubicSpline(self.x_coords[:19], self.y_coords[:19]))(self.xs)
+        self.bottom_curve = (CubicSpline(self.x_coords[19:], self.y_coords[19:]))(self.xs)
         self.mean_chord_line = 0.5 * (self.top_curve + self.bottom_curve) # z-bar
+
+    def thin_airfoil_theory(self, alpha, load = '', save = ''):
+        x, phi = sp.symbols('x phi')
+
+        # i sampled a cubic spline along a sequence of x's. to make differentiating easier in sympy, we're going to fit it to a 7th degree polynomial.
+        
+        self.zbar = (np.polyfit(self.xs, self.mean_chord_line, 7) @ np.vstack([x**7, x**6, x**5, x**4, x**3, x**2, x, 1]))[0]
+
+        dzbar_dx = (sp.diff(self.zbar, x)).subs(x, (self.chord_length / 2) * (1 - sp.cos(phi)))
+
+        if (load == ''):
+            self.A0_minus_alpha = (-1 / np.pi) * sp.integrate(dzbar_dx, (phi, 0, np.pi))
+            print('A0 calculated')
+            self.A1 = (2 / np.pi) * sp.integrate(dzbar_dx * sp.cos(phi), (phi, 0, np.pi))
+            print('A1 calculated')
+            self.A2 = (2 / np.pi) * sp.integrate(dzbar_dx * sp.cos(2 * phi), (phi, 0, np.pi))
+            print('A2 calculated')
+            if (save != ''):
+                print('Saving to data/fourier_coeffs/' + save)
+                np.savez('data/fourier_coeffs/' + save, A0_minus_alpha = self.A0_minus_alpha, A1 = self.A1, A2 = self.A2)
+                print('fourier coeffs saved to data/fourier_coeffs/' + save)
+        else:
+            print('Loading from data/fourier_coeffs/')
+            data = np.load('data/fourier_coeffs/' + load + '.npz', allow_pickle = True)
+            self.A0_minus_alpha = data['A0_minus_alpha']
+            self.A1 = data['A1']
+            self.A2 = data['A2']    
+
+        self.Cl = np.pi * (2 * (self.A0_minus_alpha + alpha) + self.A1)
+        self.Cm_LE = - (self.Cl / 4) + (np.pi / 4) * (self.A2 - self.A1)
 
 class Wing:
     def __init__(self, root_airfoil, tip_airfoil, wingspan, wing_area, twist,):
@@ -31,18 +62,18 @@ class Wing:
         self.aspect_ratio = wingspan ** 2 / wing_area
         self.twist = twist
 
+class Aircraft:
+    def __init__(self, max_takeoff_weight, cruise_speed):
+        self.max_takeoff_weight = max_takeoff_weight
+        self.cruise_speed = cruise_speed
+
 # chord lengths were estimated using a three-view pic of the b29
 b29_root = Airfoil(5.5, 0.22, 0.302, 0.017, 0.302)
 b29_tip = Airfoil(2.2, 0.09, 0.30, 0.022, 0.30)
+wing = Wing(b29_root, b29_tip, 43.05, 161.3, -1 * np.pi/180)
+plane = Aircraft(133500 * 4.44822, 220 * 0.44704)
+
+density = 0.377 # kg/m^3
 
 b29_root.process_coords('data/b29_root.csv')
 b29_tip.process_coords('data/b29_tip.csv')
-
-# wingspan = 43.05 # m
-# wing_area = 161.3 # m
-# aspect_ratio = 11.5
-
-# max_takeoff_weight = 133500 * 4.44822 # N
-# cruise_speed = 220 * 0.44704 # m/s
-
-# twist = -1 * np.pi/180 # deg
